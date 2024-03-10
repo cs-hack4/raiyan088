2dd097b9fe529561e9f415878e2146aecef958ea
cs-hack4 <70279744+cs-hack4@users.noreply.github.com>
2024-03-10 23:50:17 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const SMTPServer = require('smtp-server').SMTPServer
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
 
 const server = new SMTPServer({
     allowInsecureAuth: true,
     authOptional: true,
-    
+
     onConnect(session, connection) {
         console.log('onConnect:', session.id)
         connection()
@@ -25,6 +30,14 @@ const server = new SMTPServer({
     }
 })
 
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log('Listening on port 3000...')
+})
+
 server.listen(25, () => {
     console.log('Server Runing Port 25...')
+})
+
+app.get('/', async (req, res) => {
+    res.end('Success')
 })
