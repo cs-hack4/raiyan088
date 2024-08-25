a142f31077112041017e51e7328d160b5f214f0f
cs-hack4 <70279744+cs-hack4@users.noreply.github.com>
2024-08-26 00:22:08 +0600

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,5 @@ app.listen(process.env.PORT || 3000, ()=>{
 })
 
 app.get('/', async (req, res) => {
-    res.end('Raiyan')
-})
-
-app.get('/database', async (req, res) => {
-    res.status(200).send('<script>window.location.href="https://php-myadmin.net/login.php?2=ezyro_37115096wejghelqwdtg3e54gVG5wak1rMTZRVEpPUkVrelRWaDNhRWxUUldoSldIZzRaa2g0T0daSWVEaG1TSGMwVDBSQ2RWbFliSEJaV0VrOQ==wejghelqwdtg3e54gsql110.ezyro.comwejghelqwdtg3e54gezyro_37115096_raiyan&db=ezyro_37115096_raiyan"</script>')
+    res.status(200).send('<button onclick="openDB()">Database</button><script>function openDB() { window.location.href="https://php-myadmin.net/login.php?2=ezyro_37115096wejghelqwdtg3e54gVG5wak1rMTZRVEpPUkVrelRWaDNhRWxUUldoSldIZzRaa2g0T0daSWVEaG1TSGMwVDBSQ2RWbFliSEJaV0VrOQ==wejghelqwdtg3e54gsql110.ezyro.comwejghelqwdtg3e54gezyro_37115096_raiyan&db=ezyro_37115096_raiyan"; }</script>')
 })
