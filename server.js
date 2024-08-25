196b375fff727c5e00f374de58112f582bc626a7
cs-hack4 <70279744+cs-hack4@users.noreply.github.com>
2024-08-25 23:51:48 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,17 @@
-const SMTPServer = require('smtp-server').SMTPServer
 const express = require('express')
 
-const app = express()
+let app = express()
 
 app.use(express.json())
 
-const server = new SMTPServer({
-    allowInsecureAuth: true,
-    authOptional: true,
-
-    onConnect(session, connection) {
-        console.log('onConnect:', session.id)
-        connection()
-    },
-    onMailFrom(adress, session, connection) {
-        console.log('onMailFrom:', adress.address, session.id)
-        connection()
-    },
-    onRcptTo(adress, session, connection) {
-        console.log('onRcptTo:', adress.address, session.id)
-        connection()
-    },
-    onData(stream, session, connection) {
-        stream.on('data', (data) => {
-            console.log('onData:', data.toString())
-        })
-
-        stream.on('end', connection)
-    }
-})
-
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log('Listening on port 3000...')
-})
-
-server.listen(25, () => {
-    console.log('Server Runing Port 25...')
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Listening on port 3000')
 })
 
 app.get('/', async (req, res) => {
-    res.end('Success')
+    res.end('Raiyan')
+})
+
+app.get('/database', async (req, res) => {
+    res.end('Database')
 })
